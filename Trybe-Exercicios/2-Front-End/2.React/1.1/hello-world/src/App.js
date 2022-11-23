8908d98c156b8afb66b44c4f121c3ae1a5b9cc49
Rafael de Almeida Alves <rafael.almeida.alves.2002@gmail.com>
2022-11-23 19:37:27 -0300

    fixes

---
+++ Trybe-Exercicios/2-Front-End/2.React/1.1/hello-world/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import HelloWorld from './helloWorld';
+import './App.css';
+
+function App() {
+  return <HelloWorld />
+}
+
+export default App;
---
+++ Trybe-Exercicios/2-Front-End/2.React/1.1/hello-world/src/helloWorld.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+    <section>
+      <span className='helloWorld'>Hello, world!</span>
+      <h1>OI</h1>
+    </section>
+    );
+    
+  }
+}
+
+export default HelloWorld
